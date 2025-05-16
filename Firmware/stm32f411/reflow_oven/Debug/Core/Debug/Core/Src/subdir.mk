3cbfc72e721d918ae78d903a7c57b1827e894035
La-guajolota <adriansilpa@gmail.com>
2025-05-16 07:18:40 -0600

    third_parties added | reorganization of folders

---
+++ Firmware/stm32f411/reflow_oven/Debug/Core/Debug/Core/Src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../Core/Debug/Core/Src/gui_backend.o \
+../Core/Debug/Core/Src/main.o \
+../Core/Debug/Core/Src/max6675.o \
+../Core/Debug/Core/Src/pid.o \
+../Core/Debug/Core/Src/reflow_oven_process.o \
+../Core/Debug/Core/Src/stm32f4xx_hal_msp.o \
+../Core/Debug/Core/Src/stm32f4xx_it.o \
+../Core/Debug/Core/Src/syscalls.o \
+../Core/Debug/Core/Src/sysmem.o \
+../Core/Debug/Core/Src/system_stm32f4xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ Firmware/stm32f411/reflow_oven/Debug/Drivers/ui/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/ui/images.c \
+../Drivers/ui/screens.c \
+../Drivers/ui/styles.c \
+../Drivers/ui/ui.c 
+
+OBJS += \
+./Drivers/ui/images.o \
+./Drivers/ui/screens.o \
+./Drivers/ui/styles.o \
+./Drivers/ui/ui.o 
+
+C_DEPS += \
+./Drivers/ui/images.d \
+./Drivers/ui/screens.d \
+./Drivers/ui/styles.d \
+./Drivers/ui/ui.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/ui/%.o Drivers/ui/%.su Drivers/ui/%.cyclo: ../Drivers/ui/%.c Drivers/ui/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I"/media/adrian/usb_a/Chambas/Inbiodroid/Horno_d_reflujo/Firmware/reflow_oven/Drivers/lvgl" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"/media/adrian/usb_a/Chambas/Inbiodroid/Horno_d_reflujo/Firmware/reflow_oven/Drivers/ui" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-ui
+
+clean-Drivers-2f-ui:
+	-$(RM) ./Drivers/ui/images.cyclo ./Drivers/ui/images.d ./Drivers/ui/images.o ./Drivers/ui/images.su ./Drivers/ui/screens.cyclo ./Drivers/ui/screens.d ./Drivers/ui/screens.o ./Drivers/ui/screens.su ./Drivers/ui/styles.cyclo ./Drivers/ui/styles.d ./Drivers/ui/styles.o ./Drivers/ui/styles.su ./Drivers/ui/ui.cyclo ./Drivers/ui/ui.d ./Drivers/ui/ui.o ./Drivers/ui/ui.su
+
+.PHONY: clean-Drivers-2f-ui
+
